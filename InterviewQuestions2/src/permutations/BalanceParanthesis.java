429f9b23672fe52b0abb20b378770a71c5f4f095
suketu <suketukvakharia@gmail.com>
2019-12-31 17:25:45 -0800

    In progress implementation of balance parenthesis checking-in.

---
+++ InterviewQuestions2/src/permutations/BalanceParanthesis.java
@@ -0,0 +1,32 @@
+package permutations;
+
+/**
+ * Given a string (()) Determine whether its balanced etc.
+ * TODO This is incomplete.
+ */
+public class BalanceParanthesis {
+
+    public boolean isBalanced(String str) {
+        if (str == null || str.isEmpty()) {
+            return true;
+        }
+
+        int openParnthesis = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') {
+                openParnthesis++;
+            } else if (str.charAt(i) == ')') {
+                openParnthesis--;
+            }
+            if (openParnthesis < 0) {
+                return false;
+            }
+        }
+
+        if (openParnthesis > 0) {
+            return false;
+        }
+
+        return true;
+    }
+}
