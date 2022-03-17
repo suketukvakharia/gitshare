aa17b69446a4f3603ebf88766e5abb3d11cc608c
Suketu Vakharia <suketukv@fb.com>
2022-03-16 23:28:58 -0700

    Adding TwoSum implentation.

---
+++ InterviewQuestions2/src/math/TwoSum.java
@@ -0,0 +1,36 @@
+package math;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class TwoSum {
+    /**
+     * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+     *
+     * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+     *
+     * You can return the answer in any order.
+     *
+     * https://leetcode.com/problems/two-sum/
+     *
+     * Tested this online.
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numToIndex = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            Integer otherIndex = numToIndex.get(target - nums[i]);
+            if (otherIndex != null) {
+                return new int[] {i, otherIndex};
+
+            }
+            numToIndex.put(nums[i], i);
+        }
+
+        return new int[2];
+    }
+}
